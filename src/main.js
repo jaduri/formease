d29a4efdc3b301fe67d630a1ef45b4a7711ff195
Joseph <tjaduwari@gmail.com>
2021-01-08 12:47:36 +1030

    refactor js, code cleanup

--- src/main.js
+++ src/main.js
@@ -7,39 +7,41 @@ const form = new Form("/register", "Submit", "POST");
 //ensure document is loaded
 document.addEventListener("DOMContentLoaded", function(){
   //attach listener to add form button
-  document.getElementById("primary-form").addEventListener("submit", function(){
-    event.preventDefault();
-    //create single element from add form fields
-    createElement(this.children[1].value, this.children[3].value, this.children[5].value);
-    //reset add form fields
-    this.children[1].value = "";
-    this.children[3].value = "";
-    this.children[5].value = "";
-    //generate form to build final html form array fields
-    document.getElementById("root").innerHTML = form.generateForm();
-  });
+  document.getElementById("primary-form").addEventListener("submit", addInput);
   //add copy button event listener
-  document.getElementById("copy-button").addEventListener("click", function(){
-    event.preventDefault();
-    //store form code in variable
-    const code = document.getElementById("root").innerHTML;
-    //copy code to clipboard
-    navigator.clipboard.writeText(code).then(()=>{
-      let note = document.getElementById("notification");
-      //show notification
-      note.innerHTML = "Copied!";
-      note.classList.add("show-note");
-      const displayNote = setTimeout(function(){
-        //hide notification
-        note.classList.remove("show-note");
-        clearTimeout(displayNote);
-      }, 5000);
-    });
-  });
+  document.getElementById("copy-button").addEventListener("click", copyGeneratedHtml);
 });
 
 function createElement(type, name, label){
   type = (type === "") ? "text" : type
   label = (label === "") ? "No label" : label
   form.addField(type, name, label);
+}
+function addInput(){
+  event.preventDefault();
+  //create single element from add form fields
+  createElement(this.children[1].value, this.children[3].value, this.children[5].value);
+  //reset add form fields
+  this.children[1].value = "";
+  this.children[3].value = "";
+  this.children[5].value = "";
+  //generate form to build final html form array fields
+  document.getElementById("root").innerHTML = form.generateForm();
+}
+function copyGeneratedHtml(){
+  event.preventDefault();
+  //store form code in variable
+  const code = document.getElementById("root").innerHTML;
+  //copy code to clipboard
+  navigator.clipboard.writeText(code).then(()=>{
+    let note = document.getElementById("notification");
+    //show notification
+    note.innerHTML = "Copied!";
+    note.classList.add("show-note");
+    const displayNote = setTimeout(function(){
+      //hide notification
+      note.classList.remove("show-note");
+      clearTimeout(displayNote);
+    }, 5000);
+  });
 }
